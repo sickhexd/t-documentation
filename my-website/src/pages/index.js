a736a9de66290396b743927e698ea6320da936fe
Антонов Семен <sabiaflex0@gmail.com>
2026-05-04 21:11:22 +0700

    Деплой документации

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Redirect} from '@docusaurus/router';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Home() {
-  return <Redirect to="/docs/intro" />;
+  const introUrl = useBaseUrl('/docs/intro');
+
+  return <Redirect to={introUrl} />;
 }
